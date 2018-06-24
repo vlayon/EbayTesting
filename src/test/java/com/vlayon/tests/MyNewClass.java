f796976fc4bd12031284cc3ed942daa15d8377ba
Vladimir <vlayon@gmail.com>
2018-06-25 00:46:57 +0300

    Adding actions Before and After each tests and successfully starting Chrome.

--- src/test/java/com/vlayon/tests/MyNewClass.java
+++ src/test/java/com/vlayon/tests/MyNewClass.java
@@ -1,4 +1,34 @@
 package com.vlayon.tests;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
 public class MyNewClass {
+private static WebDriver driver;
+
+    @BeforeSuite
+    public void BeforeSuite() {
+        System.setProperty("webdriver.chrome.driver", "D:\\Setup\\CromeDriver\\chromedriver.exe");
+        driver= new ChromeDriver();
+        driver.get("https://www.ebay.co.uk/");
+    }
+
+    @Test
+    public void FirstTest() {
+        WebElement searchField= driver.findElement(By.id("gh-ac"));
+        searchField.click();
+        searchField.sendKeys("mudguard bicycle");
+        WebElement goButton=driver.findElement(By.id("gh-btn"));
+        goButton.click();
+
+    }
+    @AfterTest
+    public void AfterTest() {
+        driver.quit();
+    }
 }
